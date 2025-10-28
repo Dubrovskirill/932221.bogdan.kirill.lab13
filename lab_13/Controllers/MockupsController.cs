2e7d860fa22617ebce778e7e147ee4ff87049e03
Dubrovskirill <zet10set11@gmail.com>
2025-10-28 16:53:50 +0700

    Update

---
+++ lab_13/Controllers/MockupsController.cs
@@ -0,0 +1,63 @@
+﻿using lab_13.Models;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace lab_13.Controllers
+{
+    public class MockupsController : Controller
+    {
+        private readonly ILogger<MockupsController> _logger;
+
+        public MockupsController(ILogger<MockupsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Quiz()
+        {
+            TaskModel model = new TaskModel();
+            model.generateNewTask();
+            CollectionTasks.Instance().add(model);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Quiz(string result, string action)
+        {
+            if (string.IsNullOrWhiteSpace(result))
+            {
+                // Redisplay the current task if no answer was provided
+                var currentTask = CollectionTasks.Instance().getList().LastOrDefault();
+                return View(currentTask);
+            }
+            CollectionTasks.Instance().getList().Last().result = result;
+            CollectionTasks.Instance().checkLast();
+
+            if (action == "Finish")
+                return RedirectToAction("QuizResult");
+
+
+            TaskModel model = new TaskModel();
+            model.generateNewTask();
+            CollectionTasks.Instance().add(model);
+
+            return View(model);
+        }
+
+        public IActionResult QuizResult()
+        {
+            return View(CollectionTasks.Instance());
+        }
+
+    }
+}
